e3695dcfb191da7d06f6e148ad35aa64d9926fe4
infinity <infinity@tutorabc.com.cn>
2020-04-27 15:32:00 +0800

    update

--- app/src/main/java/cn/mycommons/miitmdidlib/MainActivity.kt
+++ app/src/main/java/cn/mycommons/miitmdidlib/MainActivity.kt
@@ -1,12 +1,15 @@
 package cn.mycommons.miitmdidlib
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bun.miitmdid.core.JLibrary
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        JLibrary.InitEntry(this.applicationContext)
     }
 }
